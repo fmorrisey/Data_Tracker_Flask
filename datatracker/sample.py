cc5925cf9d6e288423b72212e1569bc82094113a
crobtech <crob2099@gmail.com>
2020-12-16 09:25:25 -0500

    Modified details, index and results.html, Created Doughnut Chart to display individual game sales and added label to chart. Also, added links to connect the details page back to search games home

--- datatracker/sample.py
+++ datatracker/sample.py
@@ -22,7 +22,7 @@ def search_requests():
     results, hits = srch.searchByName(game_Data, search_term)
     #userResults = json.dumps(results)
     #session['userResults'] = userResults
-    return render_template('sample/results.html', res=results, hits=hits)
+    return render_template('sample/results.html', res=results, hits=hits, num=1)
 
 
 @bp.route('/search/details/<game_id>', methods=['GET'])
