91aa24b4b720a50644e7e0c355333d5841b2a99a
Forrest Morrisey <forrestmorrisey@gmail.com>
2020-12-16 21:17:33 -0600

    Search will handle null entires and no returns

--- datatracker/search.py
+++ datatracker/search.py
@@ -12,12 +12,13 @@ class search:
 
     def searchByName(game_data, search_term):
         results = []
+        if search_term is not '':
+            for game in game_data:
+                if search_term.casefold() in game.name.casefold():
+                    results.append(game)
 
-        for game in game_data:
-            if search_term.casefold() in game.name.casefold():
-                results.append(game)
-
-        return results, len(results)
+            return results, len(results)
+        else: return results, 0
 
     def searchByID(results, game_ID):  # search work
         idvResults = []
