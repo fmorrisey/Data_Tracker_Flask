5a759dfcc26e5541fa73506b490113197f1b9052
crobtech <crob2099@gmail.com>
2020-12-16 21:57:41 -0500

    Created new variables to display 'salaesPer' keys and values. Also, created doughnu chart to display copies of games sold per console.t

--- datatracker/sample.py
+++ datatracker/sample.py
@@ -30,7 +30,14 @@ def sales():  # Index.html
     game_Data = api.requests_NameSpace("https://api.dccresource.com/api/games")
     salesPer = sds.salesPer_Global(game_Data, 2013, 2020)
 
-    return render_template('/sample/sales.html', salesPer=salesPer)
+    key_list = []
+    value_list = []
+
+    for k, v in salesPer.items():
+        key_list.append(k)
+        value_list.append(v)
+
+    return render_template('/sample/sales.html', salesPer=salesPer, key_list=key_list, value_list=value_list)
 
 @bp.route('/')  # URL Route
 def analysis():  # Index.html
